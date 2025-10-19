929f2cb57b1ba7c3944dabab61aa284b7eeea95e
hosseinandjava <ameri.h22@gmail.com>
2025-10-19 20:07:52 +0330

    Update and rename One.java to Person.java
    change name of the file and also adding to property and usable constructor method

---
+++ src/com/ameri/hossein/Person.java
@@ -0,0 +1,23 @@
+class Person{
+private String name;
+private String lastName;
+  
+
+pulibc Person(String name,String lastName){
+
+  this.name = name;
+  this.lastName = lastName;
+  
+}
+
+
+@override
+public String toString(){
+
+return  name + " " + lastName;
+}
+
+
+
+
+}
