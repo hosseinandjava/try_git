1bb984eb6d6087493b13b4bdff16586939fa0e87
hosseinandjava <ameri.h22@gmail.com>
2025-10-19 19:24:42 +0330

    Update Main.java
    adding first comment in Main.java in line 8

--- src/com/ameri/hossein/Main.java
+++ src/com/ameri/hossein/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println(" This is the first try to use git");
+        /* This line added in github */
     }
 
 }
