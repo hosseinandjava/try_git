e0e862775f83c93060ecd70bd2b936c81fa6c2c3
hosseinandjava <ameri.h22@gmail.com>
2025-10-19 20:33:25 +0330

    Update Main.java
    creating an object and printing it's content

--- src/com/ameri/hossein/Main.java
+++ src/com/ameri/hossein/Main.java
@@ -6,7 +6,13 @@ public class Main {
 
         System.out.println(" This is the first try to use git");
         /* This line added in github */
- /* This line added in netbeans */
+        /* This line added in netbeans */
+
+        Person p = new Person("hossein","ameri");
+
+        Sytem.out.println(p.toString);
+
+        
     }
 
 }
