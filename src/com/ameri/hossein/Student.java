736dd5f6d39ae30551525ffb99f6fb2a6f874fc3
Hossein Ameri <ameri.h22@gmail.com>
2025-10-19 23:17:44 +0330

    working on Student file

--- src/com/ameri/hossein/Student.java
+++ src/com/ameri/hossein/Student.java
@@ -1,12 +1,34 @@
 package com.ameri.hossein;
 
-puclic class Student {
+public class Student {
 
+    private String name;
+    private String lastName;
 
+    public Student() {
 
-public Student(){
+    }
 
-}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + lastName;
+
+    }
 
 }
