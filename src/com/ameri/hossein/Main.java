5e75cf57df4eb5e4e466b11442cf226e6ba7329f
Hossein Ameri <ameri.h22@gmail.com>
2025-10-19 23:08:43 +0330

    there was to errors in Mian.java. mistaking in writting System.out.println and in calling toString method.

--- src/com/ameri/hossein/Main.java
+++ src/com/ameri/hossein/Main.java
@@ -6,13 +6,12 @@ public class Main {
 
         System.out.println(" This is the first try to use git");
         /* This line added in github */
-        /* This line added in netbeans */
+ /* This line added in netbeans */
 
-        Person p = new Person("hossein","ameri");
+        Person p = new Person("hossein", "ameri");
 
-        Sytem.out.println(p.toString);
+        System.out.println(p.toString());
 
-        
     }
 
 }
